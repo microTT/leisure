3d563c823b3c0eaf44f2173226ad0b660554fe75
tongtong.ctt <tongtong.ctt@alibaba-inc.com>
2021-12-08 19:05:24 +0800

    feat: init project

---
+++ leisure/leisureApp.swift
@@ -0,0 +1,17 @@
+//
+//  leisureApp.swift
+//  leisure
+//
+//  Created by 在今 on 2021/12/8.
+//
+
+import SwiftUI
+
+@main
+struct leisureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
